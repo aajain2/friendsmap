e8715e92fdaf407a976077ff7543efe38aa98f9f
Delali <dbruce@stanford.edu>
2026-03-12 22:49:13 -0700

    delali edits

--- src/components/landing/ActivityDropdown.tsx
+++ src/components/landing/ActivityDropdown.tsx
@@ -7,6 +7,7 @@ const ACTIVITIES = [
   'Coterm',
   'Gap Year',
   'Premed',
+  'Undergrad',
   'Other',
 ];
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -13,6 +13,7 @@ export type Activity =
   | 'Coterm'
   | 'Gap Year'
   | 'Premed'
+  | 'Undergrad'
   | 'Other'
   | '';
 
